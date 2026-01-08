3bdc17026e4272bde33ed38a072976afd68f608e
Sarthak Kapila <sarthakk@advantageclub.ai>
2026-01-08 12:36:20 +0530

    fix: schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,33 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2026_01_08_064952) do
+  create_table "blogs", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.integer "org_id", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["org_id"], name: "index_blogs_on_org_id"
+  end
+
+  create_table "orgs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.integer "org_id", null: false
+    t.string "password"
+    t.datetime "updated_at", null: false
+    t.index ["org_id"], name: "index_users_on_org_id"
+  end
+
+  add_foreign_key "blogs", "orgs"
+  add_foreign_key "users", "orgs"
+end
